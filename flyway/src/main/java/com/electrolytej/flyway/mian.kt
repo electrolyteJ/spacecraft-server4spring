e2e87bd805973eb55ef14eabead9c614668776d9
electrolytej <electrolytej@gmail.com>
2025-04-02 13:53:38 +0800

    u

--- flyway/src/main/java/com/electrolytej/flyway/mian.kt
+++ flyway/src/main/java/com/electrolytej/flyway/mian.kt
@@ -1,4 +1,4 @@
-package com.hawksjamesf.flyway
+package com.electrolytej.flyway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
